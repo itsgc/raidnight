01f646bd872c25e56dc1ab6ffecda5529226ddb5
G.C <855062+itsgc@users.noreply.github.com>
2020-12-20 00:18:12 +0000

    ISSUE-4 - basic API client library and test script

---
+++ blizz_test.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+from blizzard import BlizzardTools
+
+if __name__ == "__main__":
+    auth_data = {"scope": "wow.profile",
+                 "client_id": os.environ.get("BLIZZARD_CLIENT_ID"),
+                 "client_secret": os.environ.get("BLIZZARD_CLIENT_SECRET")}
+
+blizztools = BlizzardTools(auth_data)
+# print(blizztools.get_character_profile(realm="sunstrider", char_name="karmik"))
+print(json.dumps(blizztools.get_guild_roster(realm="boulderfist", guild_name="limited-edition")))
---
+++ blizzard.py
@@ -0,0 +1,72 @@
+import requests
+
+
+class BlizzardTools():
+    """ Class of tools to facilitate authenticating and fetching basic information
+        from Blizzard Entertainment WoW Profile API"""
+
+    def __init__(self, auth_data):
+        self.description = 'Consumes Blizzard WoW Profile API'
+        self.domain = "api.blizzard.com"
+        self.auth_domain = "battle.net"
+        self.region = auth_data.get('region', "eu")
+        self.scope = auth_data['scope']
+        self.base_url = f"https://{self.region}.{self.domain}"
+        self.auth_url = f"https://{self.region}.{self.auth_domain}"
+        self.client_id = auth_data['client_id']
+        self.client_secret = auth_data['client_secret']
+        self.headers = {
+            'Content-Type': 'application/json',
+        }
+        self.jar = requests.cookies.RequestsCookieJar()
+        self.session = requests.Session()
+        self.auth = requests.auth.HTTPBasicAuth(self.client_id, self.client_secret)
+        self.client_token = None
+
+    def _get(self, url=None, parameters=None, auth=None, response="json"):
+        r = self.session.get(url=url, params=parameters, auth=auth, headers=self.headers,
+                             cookies=self.jar)
+        if response == "text":
+            return r.text
+        elif response == "json":
+            return r.json()
+
+    def _post(self, url=None, parameters=None, payload=None, response="json"):
+        if parameters is None:
+            r = self.session.post(url=url, headers=self.headers, cookies=self.jar,
+                                  data=payload)
+        else:
+            r = self.session.post(url=url, headers=self.headers,
+                                  cookies=self.jar, params=parameters,
+                                  data=payload)
+        if response == "text":
+            return r.text
+        elif response == "json":
+            return r.json()
+
+    def get_auth_token(self):
+        url = f"{self.auth_url}/oauth/token"
+        parameters = {"grant_type": "client_credentials"}
+        self.client_token = self._get(url, auth=self.auth, parameters=parameters)
+        self.headers['Authorization'] = f'Bearer {self.client_token["access_token"]}'
+        return self.client_token
+
+    def get_character_profile(self, realm, char_name):
+        url = f"{self.base_url}/profile/wow/character/{realm}/{char_name}"
+        if self.client_token is None:
+            self.get_auth_token()
+        parameters = {"namespace": f"profile-{self.region}",
+                      "locale": "en_US",
+                      "access_token": self.client_token["access_token"]}
+        profile = self._get(url, parameters=parameters)
+        return profile
+
+    def get_guild_roster(self, realm, guild_name):
+        url = f"{self.base_url}/data/wow/guild/{realm}/{guild_name}/roster"
+        if self.client_token is None:
+            self.get_auth_token()
+        parameters = {"namespace": f"profile-{self.region}",
+                      "locale": "en_US",
+                      "access_token": self.client_token["access_token"]}
+        roster = self._get(url, parameters=parameters)
+        return roster
